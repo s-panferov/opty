a46dda1086701fae09de73a28a61401af62f395f
Damien Rajon <damien@parml-dra.dashlane.com>
2018-04-27 16:27:22 +0200

    Added match

--- index.ts
+++ index.ts
@@ -1,4 +1,11 @@
+export interface MatchPattern<T, S, N> {
+  some: (_: T) => S
+  none: (() => N) | N
+}
+
 export interface Option<T> {
+    match<S, N>(p: MatchPattern<T, S, N>): S | N;
+
     /**
      * Returns true if the option is a Some value
      */
@@ -113,6 +120,10 @@ export class Some<T> implements Option<T> {
         return new Some(fn(this.value))
     }
 
+    match<S, N>(p: MatchPattern<T, S, N>): S {
+        return p.some(this.value)
+    }
+
     isSome(): boolean {
         return true;
     }
@@ -182,6 +193,14 @@ export class None<T> implements Option<T> {
 
     constructor() {
 
+    }
+
+    match<S, N>(p: MatchPattern<never, S, N>): N {
+        if (typeof p.none === 'function') {
+            return p.none();
+        } else {
+            return p.none;
+        }
     }
 
     map <U>(fn: (a: T) => U): Option<U> {
